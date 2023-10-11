8ba3166d5b7fa1504afbdef7eaa0db7eaa6db670
Undoe97 <141639812+Undoe97@users.noreply.github.com>
2023-10-11 14:13:14 +0900

    test

---
+++ DataStructure/CircularDDL.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+typedef int Element;
+
+typedef struct LNode 
+{
+	Element Data;
+	Node* Prev;
+	Node* Next;
+}Node;
+
+class DLinkedList 
+{
+public:
+
+
+};
+
+int main() 
+{
+	
+
+
+	return 0;
+}
